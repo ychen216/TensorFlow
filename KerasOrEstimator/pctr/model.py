90ca673fcd23621ed30e47e2a129af7a9735b331
lihaonan <lihaonan@kuaishou.com>
2018-08-14 19:54:56 +0800

    success

--- KerasOrEstimator/pctr/model.py
+++ KerasOrEstimator/pctr/model.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 
 file_list = ['hdfs://default/home/rl/reco/samples/20180808/0000/nonterminal.tfrecords/part-r-00070', ]
-feature_columns = [tf.feature_column.numeric_column(key='CC_pctr_list'), ]
+feature_columns = [tf.feature_column.numeric_column(key='CC_pctr_list'),tf.feature_column.numeric_column(key='Cis_click_reward'), ]
 
+
+feature_columns_nolabel = [tf.feature_column.numeric_column(key='CC_pctr_list'), ]
 
 def input_fn():
     dataset = tf.data.TFRecordDataset(filenames=file_list)
     # tf.parse_example 接收的是是batch_examples 所以必须先对dataset做batch操作
-    dataset = dataset.shuffle(1000).repeat().batch(batch_size=32)
+    dataset = dataset.shuffle(500).repeat().batch(batch_size=32)
     def parse_example(serialized_example):
         feature = tf.parse_example(
             serialized_example,
@@ -16,7 +18,7 @@ def input_fn():
         # pctr = feature['CC_pctr_list']
         #
         # label = feature['Cis_click_reward']
-        return feature
+        return feature, feature.pop('Cis_click_reward')
 
     dataset = dataset.map(parse_example)
     dataset = dataset.make_one_shot_iterator()
@@ -29,25 +31,26 @@ def bulid_model(features, params):
     for units in params['units']:
         layer = tf.layers.dense(layer, units=units, activation=tf.nn.relu)
     # output layer
-    logits = tf.layers.dense(layer, 1, activation=None)
+    logits = tf.layers.dense(layer, params['class_num'], activation=None)
     return logits
 
 
-def model_fn(features, mode, params):
+def model_fn(features, labels, mode, params):
 
     logits = bulid_model(features, params)
-    labels = tf.zeros((32,1),dtype=tf.int32)
-    print(features,labels,logits)
+    labels = tf.reshape(labels,[-1])
+    labels = tf.cast(labels, tf.int32)
     # loss = tf.nn.sigmoid_cross_entropy_with_logits(labels=labels, logits=logits)
+    
     loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-    print(loss)
     if mode == tf.estimator.ModeKeys.TRAIN:
 
         op = tf.train.AdamOptimizer().minimize(loss=loss, global_step=tf.train.get_global_step())
         return tf.estimator.EstimatorSpec(mode, loss=loss, train_op=op)
 
     if mode == tf.estimator.ModeKeys.EVAL:
-        accuracy = tf.metrics.accuracy(labels=labels, predictions=logits)
+        predictions = tf.argmax(logits, 1)
+        accuracy = tf.metrics.accuracy(labels=labels, predictions=predictions)
         metrics = {'accuracy': accuracy}
         return tf.estimator.EstimatorSpec(mode, loss=loss, eval_metric_ops=metrics)
 
@@ -58,7 +61,8 @@ def main():
         model_fn=model_fn,
         params={
             'units': [20, 10],
-            'feature_columns': feature_columns,
+            'feature_columns': feature_columns_nolabel,
+            'class_num': 2,
         }
     )
     model.train(input_fn=input_fn, steps=1000)
