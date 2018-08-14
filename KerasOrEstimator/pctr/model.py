507f03e3ac10c6109021eff8ce5d7aaaa4615b0f
ychen <ychen2167@gmail.com>
2018-08-14 17:05:28 +0800

    change

--- KerasOrEstimator/pctr/model.py
+++ KerasOrEstimator/pctr/model.py
@@ -1,6 +1,34 @@
 import tensorflow as tf
 
+file_list = ['hdfs://default/home/rl/reco/samples/20180808/0000/nonterminal.tfrecords/part-r-00070', ]
+feature_columns = [tf.feature_column.numeric_column(key='CC_pctr_list'), ]
 
+def input_fn():
+    dataset = tf.data.TFRecordDataset(filenames=file_list)
+
+    def parse_example(serialized_example):
+        feature = tf.parse_example(
+            serialized_example,
+            features=tf.feature_column.make_parse_example_spec(feature_columns)
+        )
+        # pctr = feature['CC_pctr_list']
+        #
+        # label = feature['Cis_click_reward']
+        return feature
+
+    dataset = dataset.map(parse_example)
+    dataset = dataset.shuffle(1000).repeat().batch(batch_size=32).make_one_shot_iterator()
+    return dataset.get_next()
+
+def bulid_model(features, params):
+    # input layer
+    layer = tf.feature_column.input_layer(features, feature_columns=params['feature_columns'])
+    # hidden layers
+    for units in params['units']:
+        layer = tf.layers.dense(layer, units=units, activation=tf.nn.relu)
+    # output layer
+    logits = tf.layers.dense(layer, 1, activation=None)
+    return logits
 
 
 def model_fn(features, labels, mode, params):
